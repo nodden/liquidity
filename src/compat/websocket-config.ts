34b998e37b818e15b87c458983f3d8f0fa5cb2ad
bfu4 <bella@sleeeepyti.me>
2021-02-21 20:40:18 -0500

    even more clean up and docs

--- src/compat/websocket-config.ts
+++ src/compat/websocket-config.ts
@@ -2,14 +2,29 @@ import { AuthCredentials } from "../types/serviceprovider";
 import { HttpProvider } from 'web3-core';
 import { HttpAgent, HttpHeader } from "web3-core-helpers";
 
+/**
+ * Base timeout in milliseconds given by the Web3 documentation
+ */
 export const BASE_TIMEOUT_MS = 30000;
 
+/**
+ * Interface containing the structure of a simple Http websocket configuration
+ */
 export interface WebsocketConfig {
 
+    /**
+     * Timeout
+     */
     timeout?: number;
 
+    /**
+     * Headers
+     */
     headers?: HttpHeader[];
 
+    /**
+     * Websocket client configuration options
+     */
     clientConfig?: {
         keepalive: boolean,
         keepaliveInterval: number,
@@ -17,19 +32,45 @@ export interface WebsocketConfig {
         maxReceivedMessageSize: number
     };
 
+    /**
+     * Websocket reconnect options
+     */
     reconnect?: { auto: boolean, delay: number, maxAttempts: number, onTimeout: boolean };
 
+    /**
+     * Http agent
+     */
     agent?: HttpAgent;
 
 }
 
+/**
+ * Builder for this config
+ */
 export class WebsocketConfigBuilder {
 
+    /**
+     * Websocket url
+     * @private
+     */
     private readonly url: string;
 
-    private timeout: number = 0;
+    /**
+     * Timeout for the websocket
+     * @private
+     */
+    private timeout: number;
+
+    /**
+     * Headers
+     * @private
+     */
     private headers: HttpHeader[];
 
+    /**
+     * Configuration for the websocket client
+     * @private
+     */
     private clientConfig: {
         keepalive: boolean,
         keepaliveInterval: number,
@@ -37,38 +78,83 @@ export class WebsocketConfigBuilder {
         maxReceivedMessageSize: number
     };
 
+    /**
+     * Reconnect options
+     * @private
+     */
     private reconnect: { auto: boolean, delay: number, maxAttempts: number, onTimeout: boolean };
+
+    /**
+     * Http agent
+     * @private
+     */
+    private agent: HttpAgent;
 
     private conf : WebsocketConfig;
 
 
+    /**
+     * Create a config builder
+     *
+     * @param url url for the service
+     */
     constructor(url: string) {
         this.url = url;
+        this.timeout = BASE_TIMEOUT_MS;
     }
 
-    public addTimeout(ms: number) {
+    /**
+     * Timeout in milliseconds for the websocket
+     *
+     * @param ms timeout
+     */
+    public addTimeout(ms: number) : WebsocketConfigBuilder {
         this.timeout = ms;
+        return this;
     }
 
-    public addHeaders(headers: HttpHeader) {
+    /**
+     * Add headers to the websocket
+     *
+     * @param headers headers to add
+     */
+    public addHeaders(headers: HttpHeader) : WebsocketConfigBuilder {
         this.headers.push(headers);
+        return this;
     }
 
-    public addAuthCredentials(ac: AuthCredentials) {
+    /**
+     * Add authentication credentials for the websocket
+     *
+     * @param ac auth creds
+     */
+    public addAuthCredentials(ac: AuthCredentials) : WebsocketConfigBuilder {
         this.addHeaders({ name: 'authorization', value: ac.username + ":" + ac.password });
+        return this;
     }
 
+    public addAgent(agent: HttpAgent) : WebsocketConfigBuilder {
+        this.agent = agent;
+        return this;
+    }
+
+    /**
+     * Get an HttpProvider
+     */
     httpProvider() : HttpProvider {
         return new HttpProvider(this.url, this.build());
     }
 
+    /**
+     * Build a configuration
+     */
     public build() : WebsocketConfig {
         this.conf = {
             timeout: this.timeout,
             headers: this.headers,
             clientConfig: this.clientConfig,
             reconnect: this.reconnect,
-            agent: {}
+            agent: this.agent
         }
         return this.conf;
     }
--- src/types/serviceprovider.ts
+++ src/types/serviceprovider.ts
@@ -3,6 +3,9 @@ import { WebsocketConfig } from '../compat/websocket-config';
 
 const Web3WsProvider = require('web3-providers-ws');
 
+/**
+ * Basic auth credentials (header) structure
+ */
 export interface AuthCredentials {
 
     username: string;
@@ -10,6 +13,9 @@ export interface AuthCredentials {
 
 }
 
+/**
+ * Requirements for an HttpNetwork for `organization`
+ */
 export interface HttpNetwork {
     name: string;
     httpUrl: string;
@@ -17,18 +23,42 @@ export interface HttpNetwork {
 }
 
 
+/**
+ * Easy ServiceProvider to handle basic web3 instance needs
+ */
 export default class ServiceProvider {
 
+    /**
+     * Provider identifier
+     */
     public readonly identifier: string;
+
+    /**
+     * Http url for a 'network'
+     */
     public readonly httpUrl: string;
+
+    /**
+     * Configurations for the websocket
+     */
     public config : WebsocketConfig;
 
+    /**
+     * Create a service provider
+     *
+     * @param identifier provider identifier
+     * @param httpUrl url for the service
+     * @param config configurations for the websocket to the service
+     */
     constructor(identifier: string, httpUrl: string, config? : WebsocketConfig) {
         this.identifier = identifier;
         this.httpUrl = httpUrl;
         this.config = config;
     }
 
+    /**
+     * Get a Web3WsProvider from given configurations and given http url
+     */
     public provider() : typeof Web3WsProvider {
         if (this.config !== undefined) {
             return new Web3WsProvider(this.httpUrl, this.config)
@@ -36,6 +66,9 @@ export default class ServiceProvider {
         return Web3.givenProvider;
     }
 
+    /**
+     * Get a web3 instance
+     */
     public web3() : Web3 {
         return new Web3(this.provider());
     }
